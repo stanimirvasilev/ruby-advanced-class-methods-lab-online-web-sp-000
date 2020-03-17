a4fbbea14584cbbd7add2aa21bd61b613dd51151
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-17 13:27:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,9 +37,8 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-     if self.find_by_name
-       song_name
-     else self.create_by_name
+     if self.find_by_name(song_name)
+     else self.create_by_name(song_name)
    end
      
    end
